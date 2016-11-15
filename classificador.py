1310ec77691fe8068279756bded02f953f72f009
Alexandre Gomes <alegomes@gmail.com>
2016-11-15 18:25:24 -0200

    um monte de coisas

--- classificador.py
+++ classificador.py
@@ -1,15 +1,117 @@
 import csv
+import json
+import re
 import nltk
+import sys
+import collections
+
+# CATEGORIES = { PRO: 'pro', CONTRA: 'contra', INDIF: 'indiferente' }
+CATEGORIES = [ 'pro', 'contra', 'indiferente' ]
+
 from nltk.classify import apply_features
 
+# print('bagofwords=',bag_of_words('um homem no ônibus nos perguntou se o motivo de estarmos de preto era o golpe.'))
+#
+# sentence= um homem no ônibus nos perguntou se o motivo de estarmos de preto era o golpe.
+# words= ['um', 'homem', 'no', 'ônibus', 'nos', 'perguntou', 'se', 'o', 'motivo', 'de', 'estarmos', 'de', 'preto', 'era', 'o', 'golpe']
+# bagofwords= {'se': True, 'motivo': True, 'era': True, 'de': True, 'o': True, 'golpe': True, 'um': True, 'nos': True, 'ônibus': True, 'homem': True, 'estarmos': True, 'no': True, 'perguntou': True, 'preto': True}
+def bag_of_words(sentence):
+	# print('sentence=',sentence)
+	# Tira a pontuacao e quebra em array de palavras
+	words = re.sub('[\.,!?…:\n]','',sentence).split(" ")
+	# print('words=',words)
+	return dict([(word, True) for word in words])
+	# TODO1 Remover stopwords
+	# TODO2 Incluir bigramas
+
+# 15/11 18:04
+# Bag of words com stopwords
+# Planilha do GDcos
+# 	contra = 655
+# 	pro = 133
+# 	indefinido = 195
+# training_features= 736
+# test_features= 247
+# accuracy= 0.4939271255060729
+# Most Informative Features
+#                    mesmo = True           indefi : contra =     12.3 : 1.0
+#                    dando = True           indefi : contra =     12.3 : 1.0
+#                      fez = True           indefi : contra =     12.3 : 1.0
+#                    renan = True           indefi : contra =     12.3 : 1.0
+#                       12 = True              pro : contra =     11.5 : 1.0
+
+# corp.categories()
+# corp.fileids(categories=[label])
+# corp.words(fileids=[fileid])
+def label_feats_from_corpus(corp, feature_detector=bag_of_words):
+	label_feats = collections.defaultdict(list)
+	# print('label_feats=',label_feats)
+
+	
+	# Para cada categoria
+	for label in categories: # ['neg', 'pos']
+		# Encontra todos os arquivos
+		for fileid in corp.fileids(categories=[label]): # ['pos/cv939_10583.txt', ..., 'pos/cv999_13106.txt']
+			# print('  fileid=',fileid)
+			words = corp.words(fileids=[fileid]) # ['plot', ':', 'two', 'teen', 'couples', 'go', 'to', 'a', 'church', 'party']
+			feats = feature_detector(words) # {'plot':True, ':':True, 'teen':True ...}
+			# print('  feats=',feats)
+			label_feats[label].append(feats) # { 'pos' : [{'plot':True, ':':True, 'teen':True ...}, {}, {}...], 'neg' : [] }
+			# print('  label_feats=',label_feats)
+
+	return label_feats
+
+def get_features(corpora):
+	features = collections.defaultdict(list)
+
+	for category in corpora.keys(): # i.e pos, contra e indefinido
+		for corpus in corpora[category]:
+			features[category].append(bag_of_words(corpus))
+
+	return features  # { 'pos' : [{'plot':True, ':':True, 'teen':True ...}, {}, {}...], 
+					 #   'neg' : [{},{},...] }
+
+
+# Reparte as features classificadas em dois grupos: 
+# um para treinamento (75%) e outro para testes (25%).
+# Em ambos, quantidades identicas de features de cada
+# uma das categorias.
+def split_features(lfeats, split=0.75):
+	train_feats = []
+	test_feats = []
+
+	for label, feats in lfeats.items():
+		# label=contra 		len(feats)=655 (492+163)
+		# label=pro 		len(feats)=133
+		# label=indefinido  len(feats)=195
+		cutoff = int(len(feats) * split)
+
+		# os primeiros 75% de features do label, vao para treinamento
+		train_feats.extend([(feat, label) for feat in feats[:cutoff]])
+		# os 25% restantes, para teste
+		test_feats.extend([(feat, label) for feat in feats[cutoff:]])
+	
+	return train_feats, test_feats
 
 # 1. Funcao geradora de features
-def impeachment_features(sentence):
-	if 'golpe' in sentence:
-		return {'golpe' : True}
-	else:
-		return {'golpe' : False}
+# def impeachment_features(sentence):
+# 	if 'golpe' in sentence:
+# 		return {'golpe' : True}
+# 	else:
+# 		return {'golpe' : False}
 
+
+# def dump(obj):
+# 	for attr in dir(obj):
+# 		if hasattr( obj, attr ):
+# 			print( "obj.%s = %s" % (attr, getattr(obj, attr)))
+
+# from nltk.corpus import movie_reviews
+# print('movie_reviews=',dump(movie_reviews))
+# sys.exit()
+
+# print('label_feats_from_corpus=',label_feats_from_corpus('o peito do pedro é preto.'))
+# sys.exit()
 
 ####################
 # 2. Texto pre-classificado para treinamento e testes
@@ -22,85 +124,135 @@ def impeachment_features(sentence):
 # 						("@stf_oficial fracassa perante a sociedade brasileira. #somostodosgolpistas ","pro"),
 # 						("impitma sem crime é golpe ","contra"),]
 
+if __name__ == '__main__':
 
-# 2.2. Carregado a partir do CSV
-texto_classificado = []
-with open('AmostraAGOSTO - AMOSTRAAGO10003110-2.csv', newline='') as csvfile:
-	
-	# 2.2.1. Ler CSV como Array e acessar os campos atraves de indices: row[0], row[1]...
-	# reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+	# 2.2. Carregado a partir do CSV
+	classified_corpora = collections.defaultdict(list) # { 'pro' : ['texto1', 'texto2', ...], 'contra' : []...}
 
-	# 2.2.2. Ler como Dict e acessar os campos atraves de chaves: row['nome'], row['endereco']...
-	reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-
-	for row in reader:
-		# 2.2.1. 
-		# print(row[4],row[5],row[6],row[7])
-
-		# 2.2.2.
-		print(row['TEXTO'],row['PRO'],row['CONTRA'],row['INDEFINIDO'])
-
-		if int(row['PRO']):
-			opiniao = 'pro'
-		elif int(row['CONTRA']):
-			opiniao = 'contra'
-		else:
-			opiniao = 'indefinido' 
+	with open('AmostraAGOSTO - AMOSTRAAGO10003110-2.csv', newline='') as csvfile:
 		
-		# opiniao = True if row['PRO'] else False if row['CONTRA'] else Nil if row['INDEFINIDO']
-		
-		texto_classificado.append((row['TEXTO'],opiniao))
+		# 2.2.1. Ler CSV como Array e acessar os campos atraves de indices: row[0], row[1]...
+		# reader = csv.reader(csvfile, delimiter=',', quotechar='"')
 
-print('texto_classificado=',texto_classificado)
-print('len(texto_classificado)=',len(texto_classificado))
+		# 2.2.2. Ler como Dict e acessar os campos atraves de chaves: row['nome'], row['endereco']...
+		reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
 
-# 2.3. Lido diretamente a partir do Google Spreadsheet
-# TODO (ver gsheet.py)
+		for row in reader:
+			# 2.2.1. 
+			# print(row[4],row[5],row[6],row[7])
+
+			# 2.2.2.
+			# print(row['TEXTO'],row['PRO'],row['CONTRA'],row['INDEFINIDO'],bag_of_words(row['TEXTO']))
+
+			if int(row['PRO']):
+				opiniao = 'pro'
+			elif int(row['CONTRA']):
+				opiniao = 'contra'
+			else:
+				opiniao = 'indefinido' 
+			
+			# opiniao = True if row['PRO'] else False if row['CONTRA'] else Nil if row['INDEFINIDO']
+			
+			classified_corpora[opiniao].append(row['TEXTO'])
+			# texto_classificado.append((row['TEXTO'],opiniao))
+
+	# print('texto_classificado=',texto_classificado)
+	# print('len(texto_classificado)=',len(texto_classificado))
+
+	classified_features = get_features(classified_corpora)
+	# 2.3. Lido diretamente a partir do Google Spreadsheet
+	# TODO (ver gsheet.py)
+
+	# print('classified_features=',classified_features)
 
 
-####################
-# 3. Cálculo das features dos textos classificados
+	####################
+	# 3. Cálculo das features dos textos classificados
 
-# features = [ (impeachment_features(sentence),vote) for (sentence,vote) in texto_classificado ]
-# training_features = features[:4]
-# test_features = features[4:]
+	# features = [ (impeachment_features(sentence),vote) for (sentence,vote) in texto_classificado ]
+	# training_features = features[:4]
+	# test_features = features[4:]
 
-# Pra não colocar tudo na memória
-training_features = apply_features(impeachment_features, texto_classificado[:3])
-test_features = apply_features(impeachment_features, texto_classificado[3:])
+	# Pra não colocar tudo na memória
+	# training_features = apply_features(impeachment_features, classified_features[:500])
+	# test_features = apply_features(impeachment_features, classified_features[500:])
 
-# print('features=',features)
-print('training_features=',training_features)
-print('test_features=',test_features)
 
-# 1. Exemplo
-# [({:},),({:},)]
-features_treinamento = [({'golpe':True},'contra'),
-						({'golpe':False},'true')]
+	train_features, test_features = split_features(classified_features, split=0.75)
+	print('training_features=',len(train_features))
+	print('test_features=',len(test_features))
 
-# 2. Jeito obvio
-# features_treinamento = []
-# for t in texto_classificado:
-# 	text = t[0]
-# 	vote = t[1]
-# 	print('[%s] - [%s]' % (text,vote))
-# 	features_treinamento.append((impeachment_features(text),vote))
+	# print('features=',features)
+	# print('training_features=',training_features)
+	# print('test_features=',test_features)
 
-# 3. Jeito pythoniano
-# features_treinamento = [ (impeachment_features(sentence),vote) for (sentence,vote) in texto_classificado ]
+	# 1. Exemplo
+	# [({:},),({:},)]
+	# features_treinamento = [({'golpe':True},'contra'),
+	# 						({'golpe':False},'pro')]
 
-print('features_treinamento',features_treinamento)
+	# 2. Jeito obvio
+	# features_treinamento = []
+	# for t in texto_classificado:
+	# 	text = t[0]
+	# 	vote = t[1]
+	# 	print('[%s] - [%s]' % (text,vote))
+	# 	features_treinamento.append((impeachment_features(text),vote))
 
-# Criacao do classificador
-classifier = nltk.NaiveBayesClassifier.train(features_treinamento)
+	# 3. Jeito pythoniano
+	# features_treinamento = [ (impeachment_features(sentence),vote) for (sentence,vote) in texto_classificado ]
 
-# Teste de duas sentenças
-print(classifier.classify(impeachment_features('É golpe')))			# contra
-print(classifier.classify(impeachment_features('É impeachment')))	# pro
+	# print('features_treinamento',features_treinamento)
 
-# Verificação da acurácia
-print('accuracy=',nltk.classify.accuracy(classifier, test_features))
+	# Criacao do classificador
+	classifier = nltk.NaiveBayesClassifier.train(train_features)
 
-classifier.show_most_informative_features(5)
+	# Teste de duas sentenças
+	# print(classifier.classify(impeachment_features('É golpe')))			# contra
+	# print(classifier.classify(impeachment_features('É impeachment')))	# pro
 
+	# Verificação da acurácia
+	print('accuracy=',nltk.classify.accuracy(classifier, test_features))
+
+	print('most_informative_features=',len(classifier.most_informative_features()))
+	print('-------')
+	classifier.show_most_informative_features(5)
+
+	# testes
+	texto = 'RT @JFMargarida: Juiz de Fora mantém a sua tradição democrática. Praça das estação contra o golpe #GolpeAquiNaoPassa https://t.co/LfIOsDaIVj'
+	print('[%s] %s' % (classifier.classify(bag_of_words(texto)), texto))
+	texto = 'Rio de Janeiro\n\nMST tranca a Dutra\n\nNa luta contra o golpe, a memória do massacre de Eldorado dos Carajás -20 anos https://t.co/26rk7OADEJ'
+	print('[%s] %s' % (classifier.classify(bag_of_words(texto)), texto))
+	texto = 'Ñ permitiremos q corruptos governem, não daremos paz nem um dia ,perseguiremos o Temer até na hora d colocar botox! https://t.co/LsgWDbnosM'
+	print('[%s] %s' % (classifier.classify(bag_of_words(texto)), texto))
+
+	i = 0
+	# Classificar tweets
+	# json_file = open('RT_democracia_golpe_Abr2016.txt','r')
+	# json_file = open('RT_democracia_golpe_Ago2016.txt','r')
+	# for line in json_file:
+
+	# 	i += 1
+
+	# 	if line[0] == '{':
+	# 		# print('line=',line)
+
+	# 		obj = json.loads(line)
+	# 		# print('obj=',obj)
+
+	# 		id = obj['id']
+	# 		# texot = obj['']
+
+	# 		if "retweeted_status" in obj:
+	# 			id_retweetado = obj['retweeted_status']['id']
+	# 			texto_retweetado = obj['retweeted_status']['text'] 
+	# 			#.replace('"',"'").replace("\n"," - ")
+	# 			#texto = re.sub(":(\w)", "\:\\1", texto)
+	# 			# texto = re.sub("http.*?( |$)","",texto)
+	# 			# if "golp" in texto.lower():
+	# 						# if detect(texto) == 'pt':
+	# 			# texto.encode('utf-8').decode('utf-8').encode('cp1252','ignore').decode('cp1252'))
+
+	# 			# print('%s,%s,%s' % (id, id_retweetado, texto_retweetado))
+	# 			print('%d,%s,%s' % (i,texto_retweetado, classifier.classify(impeachment_features(texto_retweetado))))
 
